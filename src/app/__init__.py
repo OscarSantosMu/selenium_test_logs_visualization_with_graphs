601dd018c517063b1a4c0fa1840e7201acea14e7
Oscar Santos <oscarsantosmu@gmail.com>
2023-03-29 14:17:22 -0600

    feat: add elasticsearch & register blueprint

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,11 +1,32 @@
+import json
+
 from flask import Flask
+from elasticsearch import Elasticsearch
+
+from .config import Config
+
+
+try:
+    es = Elasticsearch(
+        {"scheme": Config.SCHEME, "host": Config.HOST, "port": Config.PORT},
+        ca_certs=Config.CA_CERTS_PATH,
+        basic_auth=("elastic", Config.ELASTIC_PASSWORD),
+    )
+    # info() method raises error if domain or conn is invalid
+    # print(json.dumps(Elasticsearch.info(es).body, indent=4), "\n")
+
+except Exception as e:
+    print("Elasticsearch() ERROR:", e, "\n")
+    es = None
 
 
 def create_app():
     app = Flask(__name__)
 
     from .main.routes import main
+    from .elasticsearch.routes import elasticsearch
 
     app.register_blueprint(main)
+    app.register_blueprint(elasticsearch)
 
     return app
