4a8191e45f31f9672ccd9d1926830413777fd1c7
Oscar Santos <oscarsantosmu@gmail.com>
2023-03-30 01:46:16 -0600

    docs: add info about utils from elastisearch

--- src/app/elasticsearch/utils.py
+++ src/app/elasticsearch/utils.py
@@ -1,9 +1,23 @@
+"""Module from elasticsearch subpackage with utilities for Elasticsearch."""
+
+__docformat__ = "google"
+
 from elasticsearch import ApiError, TransportError
 
 from .. import es
 
 
-def perform_elasticsearch_query(search_query, fields):
+def perform_elasticsearch_query(search_query: str, fields: list) -> dict:
+    """Return the Elasticsearch query results.
+
+    Args:
+        search_query: user search query
+        fields: applicable fields to search
+
+    Returns:
+        Elasticsearch query results as a dictionary.
+    """
+
     # Create the Elasticsearch query using the search query and fields
     query = {"query": {"multi_match": {"query": search_query, "fields": fields}}}
 
