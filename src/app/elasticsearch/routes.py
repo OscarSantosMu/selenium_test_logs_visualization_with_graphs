6da09420ce0d5ef037d526779340d2861ee529ea
Oscar Santos <oscarsantosmu@gmail.com>
2023-03-30 01:45:58 -0600

    docs: add info about API and elasticsearch routes

--- src/app/elasticsearch/routes.py
+++ src/app/elasticsearch/routes.py
@@ -1,7 +1,13 @@
+"""Module from elasticsearch subpackage with routes for Elasticsearch features."""
+
+__docformat__ = "google"
+
 import string
 
+import flask
 from flask import jsonify, render_template, request, url_for, Blueprint
 from elasticsearch import ApiError, TransportError
+from elastic_transport import ObjectApiResponse
 
 from .. import es
 from ..graphs.utils import read_logs
@@ -11,8 +17,12 @@ elasticsearch = Blueprint("elasticsearch", __name__)
 
 
 @elasticsearch.route("/upload")
-def upload_documents():
+def upload_documents() -> dict:
+    """Upload documents to an Elasticsearch index.
 
+    Returns:
+        Empty dictionary.
+    """
     _, df = read_logs()
 
     id = 1
@@ -45,12 +55,33 @@ def upload_documents():
 
 @elasticsearch.route("/search", methods=["POST"])
 def search():
+    """Return the search results page.
+
+    Returns:
+        Rendered template with the search results page.
+    """
     hits = search_helper()
     return render_template("query_results.html", hits=hits)
 
 
 @elasticsearch.route("/api/search", methods=["POST", "GET"])
-def api_search():
+def api_search() -> flask.Response:
+    """Return the search results as a JSON object.
+
+    Query parameters:
+        - **query**: query string.\n
+        - **day**: 1 if day is to be included in the search results, 0 otherwise.\n
+        - **mime_types**: 1 if mime_types is to be included in the search results, 0 otherwise.\n
+        - **mime_types_count**: 1 if mime_types_count is to be included in the search results, 0 otherwise.\n
+        - **status_codes**: 1 if status_codes is to be included in the search results, 0 otherwise.\n
+        - **status_codes_count**: 1 if status_codes_count is to be included in the search results, 0 otherwise.
+
+    Returns:
+        JSON object with the search results.
+
+    Examples:
+        >>> curl -X GET "http://localhost:5000/api/search?query=39day=1"
+    """
     hits = search_helper()
     for hit in hits:
         if hit["_source"]["mime_types"] != 0.0:
@@ -64,7 +95,18 @@ def api_search():
 
 
 @elasticsearch.route("/api/search/<int:id>")
-def api_search_by(id: int):
+def api_search_by(id: int) -> ObjectApiResponse:
+    """Find a document by its ID.
+
+    Args:
+        id: document ID
+
+    Returns:
+        JSON object with the document.
+
+    Examples:
+        >>> curl -X GET "http://localhost:5000/api/search/164"
+    """
 
     try:
         resp = es.get(index="logs-index", id=id)
@@ -79,13 +121,28 @@ def api_search_by(id: int):
 
 
 @elasticsearch.route("/api/delete/<int:id>")
-def api_delete_by(id: int):
+def api_delete_by(id: int) -> flask.Response:
+    """Delete a document by its ID.
 
+    Args:
+        id: document ID
+
+    Returns:
+        Empty JSON object.
+
+    Examples:
+        >>> curl -X GET "http://localhost:5000/api/delete/164"
+    """
     es.delete(index="logs-index", id=id)
     return jsonify({})
 
 
 def search_helper():
+    """Return the search results based on the request method and query parameters.
+
+    Returns:
+        Search results as a list of dictionaries or empty dict or empty list.
+    """
     # Get the search query from the form or query parameters, depending on the request method
     if request.method == "POST":
 
